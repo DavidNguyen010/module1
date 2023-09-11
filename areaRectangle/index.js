c9afbfc456193684b843cd47588dbca710dc5e96
unknown <davidnguyen010007@gmail.com>
2023-09-11 22:55:56 +0700

    add file areaRectangle

---
+++ areaRectangle/index.js
@@ -0,0 +1,9 @@
+inputWith = prompt('please type with of rectangle')
+inputHeight=prompt('please type height of rectangle');
+
+var _With=parseInt(inputWith);
+var _Height=parseInt(inputHeight);
+
+
+var areaRectangle = _With * _Height ;
+document.write('Area of Rectangle = '+areaRectangle);
