a4423b01e33c37a0a9b2640b78042b7b713bcffd
unknown <davidnguyen010007@gmail.com>
2023-09-11 23:43:59 +0700

    add file variablesAndOperator

---
+++ variablesAndOperator/index.js
@@ -0,0 +1,39 @@
+// lesson 1
+
+// var toan = prompt("Nhap diem toan di:");
+// var ly=prompt("nhap diem ly di");
+// var hoa=prompt("nhap diem hoa di");
+// diemToan = parseInt(toan);
+// diemLy = parseInt(ly);
+// diemHoa = parseInt(hoa);
+
+
+
+// var sum = (diemToan + diemLy + diemHoa);
+// var average= sum/3;
+
+// document.write("Diem trung binh : " + average);
+
+// document.write("Tong diem  : " + sum);
+
+// lesson 2
+// var nhietDo = prompt("nhap nhiet do can doi: ");
+// _Nhiet_Do = parseInt(nhietDo);
+// var F = _Nhiet_Do/5*9 +32;
+
+// document.write("Nhiet do F : " + F);
+
+// lesson 3
+const pi = 3.14;
+var radian = prompt("Nhap ban kinh:");
+radian = parseInt(radian);
+
+var areaCircle = pi*radian*radian;
+var perimeterCicle = 2*pi*radian;
+document.write("Area of the Circle = " + areaCircle);
+document.write("Perimeter of the Circle =" + perimeterCicle);
+
+
+
+
+
